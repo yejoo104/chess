e63d1b2a9cacacdc0401df6536ec7e39807615fb
한예주 <yejoo_han@college.harvard.edu>
2020-09-01 23:27:07 +0900

    prints possible moves for pawn

--- chess.cpp
+++ chess.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void printboard(vector <vector <int> > board);
+vector <vector <int> > possiblemoves (vector <vector <int> > board, int row, int col);
 
 int main(int argc, char** argv)
 {
@@ -24,6 +25,11 @@ int main(int argc, char** argv)
   int col = grid[0] - 'A';
 
   cout << "Here are possible moves:\n";
+  vector <vector <int> > possible = possiblemoves(board, row, col);
+  for (int i = 0; i < possible.size(); i++)
+  {
+    cout << char(possible[i][1] + 'A') << board.size() - possible[i][0] << endl;
+  }
 }
 
 void printboard(vector <vector <int> > board)
@@ -47,4 +53,23 @@ void printboard(vector <vector <int> > board)
     }
     cout << board.size() - i << endl;
   }
+}
+
+vector <vector <int> > possiblemoves (vector <vector <int> > board, int row, int col)
+{
+  vector <vector <int> > possible;
+
+  // Pawn (White)
+  if (board[row][col] == 1)
+  {
+    if (row - 1 >= 0 && board[row - 1][col] == 0)
+    {
+      possible.push_back({row - 1, col});
+      if (row - 2 >= 0 && board[row - 2][col] == 0) possible.push_back({row - 2, col});
+    }
+    if (col + 1 < board[0].size() && board[row - 1][col + 1] < 0) possible.push_back({row - 1, col + 1});
+    if (col - 1 >= 0 && board[row - 1][col - 1] < 0) possible.push_back({row - 1, col - 1});
+  }
+
+  return possible;
 }
