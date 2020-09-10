3ebb03bcdc61b51a5d5568b1eca0888de211ae2e
한예주 <yejoo_han@college.harvard.edu>
2020-09-10 14:31:33 +0900

    added knight moves

--- chessoop.cpp
+++ chessoop.cpp
@@ -422,5 +422,24 @@ set<vector <int> > possiblemoves (Board board, int row, int col)
     }
   }
 
+  // Knight
+  if (type == KNIGHT)
+  {
+    int xdir[8] = {2, 2, 1, 1, -1, -1, -2, -2};
+    int ydir[8] = {1, -1, 2, -2, 2, -2, 1, -1};
+
+    for (int i = 0; i < 8; i++)
+    {
+      int x = row + xdir[i];
+      int y = col + ydir[i];
+      if (x >= 0 && x < 8 && y >= 0 && y < 8)
+      {
+        Piece poten = board.getLocation(x, y).getPiece();
+        if (poten.getPiecetype() == NONE) possible.insert({x, y});
+        else if (poten.getWhite() != white) possible.insert({x, y});
+      }
+    }
+  }
+
   return possible;
 }
